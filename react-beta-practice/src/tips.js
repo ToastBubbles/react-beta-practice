03e016124c8a6b5c3cf4c6413a709a2e0aa83877
Jeffrey Neal <jeffneal11@gmail.com>
2022-12-08 08:44:03 -0600

    initialized app

---
+++ react-beta-practice/src/tips.js
@@ -0,0 +1,26 @@
+/*
+
+returns:
+
+    - must be <div>wrapped</div>
+    - placed in (parenthesis)
+
+Components:
+
+    -(function) names must be Capitalized
+
+Exports:
+
+    -export default
+        -import ComponentName from './Components/File'
+        (only one default per comp)
+
+    -export named
+        -import { ComponentName } from './Components/File'
+
+
+
+
+
+
+*/
