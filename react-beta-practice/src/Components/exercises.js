9903d24475f75df318a2ea9620b73dc3148b7baf
Jeffrey Neal <jeffneal11@gmail.com>
2023-01-04 09:23:11 -0600

    completed exercise

--- react-beta-practice/src/Components/exercises.js
+++ react-beta-practice/src/Components/exercises.js
@@ -1,5 +1,6 @@
 export default function ExerciseBody() {
-  let problemNumber = 1;
+  let problemNumber = 2;
+
   return (
     <div>
       <div>Problem Number: {problemNumber}</div>
@@ -14,7 +15,14 @@ function CalculateAnswer(problemNum) {
     case 1:
       return PrintShrimp();
     case 2:
-      return PrintShrimp();
+      return (
+        FilterListRunner(1) +
+        "" +
+        FilterListRunner(2) +
+        "" +
+        FilterListRunner(3)
+      );
+
     case 3:
       return PrintShrimp();
     case 4:
@@ -28,4 +36,32 @@ function CalculateAnswer(problemNum) {
 
 function PrintShrimp() {
   return "Shrimpers";
+}
+
+function FilterListRunner(index) {
+  let a1 = [1, 2, "a", "b"];
+  let a2 = [1, "a", "b", 0, 15];
+  let a3 = [1, 2, "aasf", "1", "123", 123];
+
+  switch (index) {
+    case 1:
+      return FilterList(a1);
+    case 2:
+      return FilterList(a2);
+    case 3:
+      return FilterList(a3);
+    default:
+      return "No solution";
+  }
+}
+
+function FilterList(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== "number") {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  console.log(arr);
+  return "[" + arr.join(",") + "]";
 }
