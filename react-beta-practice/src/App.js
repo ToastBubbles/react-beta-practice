3e7cf801af0e4c5af26a6537499a458c2d623f3d
Jeffrey Neal <jeffneal11@gmail.com>
2023-01-04 14:26:49 -0600

    made a funny box

--- react-beta-practice/src/App.js
+++ react-beta-practice/src/App.js
@@ -7,8 +7,72 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="title">Class exercises</h1>
-        <div className="Logo-container">
+        {/* <div className="Logo-container">
           <img src={logo} className="App-logo" alt="logo" />
+        </div> */}
+        <div className="Box-container">
+          <div className="Segment s1"></div>
+          <div className="Segment s2"></div>
+          <div className="Segment s3"></div>
+          <div className="Segment s4"></div>
+
+          <div className="Segment s5"></div>
+          <div className="Segment s6"></div>
+          <div className="Segment s7"></div>
+          <div className="Segment s8"></div>
+
+          <div className="Segment s9"></div>
+          {/* <svg width="500" height="500">
+            <defs>
+              <linearGradient
+                id="e"
+                x1="40"
+                y1="210"
+                x2="460"
+                y2="290"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="steelblue" offset="0" />
+                <stop stop-color="red" offset="1" />
+              </linearGradient>
+            </defs>
+            <line
+              x1="0"
+              y1="50"
+              x2="230"
+              y2="0"
+              stroke="url(#e)"
+              stroke-width="30"
+              className="line"
+            />
+            <line
+              x1="230"
+              y1="0"
+              x2="400"
+              y2="50"
+              stroke="url(#e)"
+              stroke-width="30"
+              className="line"
+            />
+            <line
+              x1="170"
+              y1="100"
+              x2="400"
+              y2="50"
+              stroke="url(#e)"
+              stroke-width="30"
+              className="line"
+            />
+            <line
+              x1="170"
+              y1="100"
+              x2="0"
+              y2="50"
+              stroke="url(#e)"
+              stroke-width="30"
+              className="line"
+            />
+          </svg> */}
         </div>
         <ExerciseBody problem={2} />
       </header>
