3db73419fa2ffbd3278eff4cf8853a3c8f93b299
Jeffrey Neal <jeffneal11@gmail.com>
2023-01-04 08:41:48 -0600

    added class exercise component

--- react-beta-practice/src/App.js
+++ react-beta-practice/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ExerciseBody from "./Components/exercises";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="title">Class exercises</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ExerciseBody />
       </header>
     </div>
   );
--- react-beta-practice/src/tips.js
+++ react-beta-practice/src/tips.js
@@ -2,7 +2,7 @@
 
 returns:
 
-    - must be <div>wrapped</div>
+    - must be <div>wrapped</div> or <> </>
     - placed in (parenthesis)
 
 Components:
@@ -14,9 +14,18 @@ Exports:
     -export default
         -import ComponentName from './Components/File'
         (only one default per comp)
+        [can import as any name]
 
     -export named
         -import { ComponentName } from './Components/File'
+
+JSX:
+
+    -attributes
+        camelCase: strokeWidth instead of stroke-width
+    -className instead of class
+
+
 
 
 
